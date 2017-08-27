ec1d117f8cf662b200312bc359df170f8a5c49b5
Joel Cornett <joecorne@amazon.com>
2017-08-27 14:52:31 -0400

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const webpack = require('webpack');
+
 const path = require('path');
+const camelCase = require('camelcase');
+
+const pkg = require('./package.json');
 
 const config = {
-  entry: {
-    index: ['bootstrap', './src/index.js'],
-  },
+  // path.join(...) strips out the leading '.', which webpack requires.
+  // FIXME: this *might* break building on non-POSIX environments (e.g. Windows)
+  entry: `./${pkg.module}`,
   output: {
-    filename: 'lofi-bootstrap-for-webpack.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'lofiBootstrapForWebpack'
+    filename: path.basename(pkg.main),
+    path: path.resolve(__dirname, path.dirname(pkg.main)),
+    library: camelCase(path.basename(pkg.main, '.js'))
   },
   externals: [
+    'jquery',
     'bootstrap',
+    'popper.js'
     // TODO: figure out path to SCSS entry point for bootstrap
     // 'bootstrap/path/to/scss/entry'
   ],
@@ -25,19 +32,32 @@ const config = {
   devServer: {
     hot: true
   },
-  plugins: []
+  plugins: [
+    new webpack.ProvidePlugin({
+      // required for bootstrap
+      jQuery: 'jquery',
+      Popper: 'popper.js'
+    })
+  ]
 };
 
 module.exports = (env) => {
+  // source: https://stackoverflow.com/a/36728755/1142167
   const isDevServer = process.argv.find(v => v.includes('webpack-dev-server'));
   if (isDevServer) {
-    const webpack = require('webpack');
     const HtmlWebpackPlugin = require('html-webpack-plugin');
+
     config.plugins.push(
       new HtmlWebpackPlugin(),
       new webpack.HotModuleReplacementPlugin()
     );
+
+    // need dependencies to be present in dev server mode
     config.externals.splice(0);
+  } else if (env && env.production) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+    );
   }
 
   return config;
