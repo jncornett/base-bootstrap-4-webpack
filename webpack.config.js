18864c0bef681eb14e6da226502c17a8a017630c
Joel Cornett <joecorne@amazon.com>
2017-08-27 14:09:21 -0400

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,44 @@
 const path = require('path');
 
-module.exports = {
+const config = {
   entry: {
-    index: './src/index.js',
+    index: ['bootstrap', './src/index.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'lofi-bootstrap-for-webpack.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'lofiBootstrapForWebpack'
   },
   externals: [
     'bootstrap',
     // TODO: figure out path to SCSS entry point for bootstrap
-    // 'bootstrap/path/to/scss/entry',
-    'font-awesome-webpack'
-  ]
+    // 'bootstrap/path/to/scss/entry'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    hot: true
+  },
+  plugins: []
+};
+
+module.exports = (env) => {
+  const isDevServer = process.argv.find(v => v.includes('webpack-dev-server'));
+  if (isDevServer) {
+    const webpack = require('webpack');
+    const HtmlWebpackPlugin = require('html-webpack-plugin');
+    config.plugins.push(
+      new HtmlWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    );
+    config.externals.splice(0);
+  }
+
+  return config;
 };
